44ff448e32e3cd0e3daca564d83226c1414c6be8
SahishnavKritin <109713816+SahishnavKritin@users.noreply.github.com>
2024-07-12 13:46:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For #### and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+def main():
+    st.title("CSV File Upload")
+
+    # File uploader widget
+    uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+    if uploaded_file is not None:
+        # Read the CSV file
+        data = pd.read_csv(uploaded_file)
+        
+        # Display the CSV file
+        st.write(data)
+
+if __name__ == "__main__":
+    main()
