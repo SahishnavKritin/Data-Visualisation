9c3fa45359d6dfd8c52c8a33c4c3d994c51b1b74
SahishnavKritin <109713816+SahishnavKritin@users.noreply.github.com>
2024-07-12 15:11:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,19 +40,24 @@ def main():
             moving_avg_window = st.number_input("Select Moving Average Window Size (4 or above)", min_value=4, step=1, value=4)
             data[selected_column] = data[selected_column].rolling(window=moving_avg_window).mean()
 
-        # Get the range of transformed data to plot
-        plot_data = data[selected_column][start_index:end_index]
-
         # Select plot segment
-        start_index = st.number_input("Start Index", min_value=0, max_value=len(plot_data)-1, step=20, value=0)
-        end_index = st.number_input("End Index", min_value=0, max_value=len(plot_data)-1, step=20, value=len(data)-1)
+        start_index = st.number_input("Start Index", min_value=0, max_value=len(data)-1, step=1, value=0)
+        end_index = st.number_input("End Index", min_value=0, max_value=len(data)-1, step=1, value=len(data)-1)
+
+        # Get the range of transformed data to plot
+        plot_data = data.iloc[start_index:end_index]
 
         # Plot the data using Matplotlib
         fig, ax = plt.subplots(figsize=(10, 6))
-        ax.plot(plot_data.index, plot_data, label=selected_column)
+        ax.plot(plot_data.index, plot_data[selected_column], label=selected_column)
         ax.set_xlabel("Index")
         ax.set_ylabel(selected_column)
         ax.set_title(f"Plot of {selected_column}")
+
+        # Set y-axis limits based on data range
+        y_min = plot_data[selected_column].min()
+        y_max = plot_data[selected_column].max()
+        ax.set_ylim(y_min, y_max)
 
         # Display plot in Streamlit
         st.pyplot(fig)
