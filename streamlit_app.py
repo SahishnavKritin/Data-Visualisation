c18f568221438301c28a3330e647f2c00e935e86
SahishnavKritin <109713816+SahishnavKritin@users.noreply.github.com>
2024-07-12 15:21:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,19 +24,11 @@ def main():
         columns = data.columns
         selected_column = st.selectbox("Select Column to Plot", columns)
 
-        # Checkbox for downsampling
-        downsample_option = st.checkbox("Apply Downsampling")
-
-        # Show downsampling options if checked
-        if downsample_option:
-            downsample_factor = st.selectbox("Select Downsampling Factor", [2, 3, 4, 6])
-            data[selected_column] = data[selected_column][::downsample_factor]
-
         # Checkbox for moving average
         moving_avg_option = st.checkbox("Apply Moving Average")
 
         # Show moving average options if checked
-        '''if moving_avg_option:
+        if moving_avg_option:
             moving_avg_window = st.number_input("Select Moving Average Window Size (4 or above)", min_value=4, step=1, value=4)
             data["moving_avg"] = data[selected_column].rolling(window=moving_avg_window).mean()
             #st.write("### Moving Average Data:")
@@ -46,7 +38,7 @@ def main():
             remove_baseline_option = st.checkbox("Remove Moving Average Baseline")
 
             if remove_baseline_option:
-                data[selected_column] = data[selected_column] - data["moving_avg"]'''
+                data[selected_column] = data[selected_column] - data["moving_avg"]
 
         # Select plot segment
         start_index = st.number_input("Start Index", min_value=0, max_value=len(data)-1, step=1, value=0)
@@ -69,6 +61,29 @@ def main():
 
         # Display plot in Streamlit
         st.pyplot(fig)
+
+        downsample_option = st.checkbox("Apply Downsampling")
+
+        # Show downsampling options if checked
+        if downsample_option:
+            downsample_factor = st.selectbox("Select Downsampling Factor", [2, 3, 4, 6])
+            data[selected_column] = data[selected_column][::downsample_factor]
+            plot_data = data.iloc[start_index:end_index]
+
+            # Plot the data using Matplotlib
+            fig, ax = plt.subplots(figsize=(10, 6))
+            ax.plot(plot_data.index, plot_data[selected_column], label=selected_column)
+            ax.set_xlabel("Index")
+            ax.set_ylabel(selected_column)
+            ax.set_title(f"Plot of {selected_column}")
+    
+            # Set y-axis limits based on data range
+            y_min = plot_data[selected_column].min()
+            y_max = plot_data[selected_column].max()
+            ax.set_ylim(y_min, y_max)
+    
+            # Display plot in Streamlit
+            st.pyplot(fig)
 
 if __name__ == "__main__":
     main()
